40283c58cccc650e20cdde0f707fbce6f62d78f1
Vladimir N. Silyaev <vsilyaev@gmail.com>
2019-06-02 19:30:53 -0700

    Added program to save mtime into simple database

--- fuse_interposer_c.c
+++ fuse_interposer_c.c
@@ -1,10 +1,13 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/mman.h>
 #include <sys/time.h>
 #include <sys/xattr.h>
 
@@ -14,17 +17,199 @@
 #define UNUSED(x) (void)(x)
 #define CHECK(call) ({int rc = call; if(rc<0) {return -errno;}rc;})
 
+struct mtimedb {
+    const char *prefix;
+    size_t prefix_len;
+    int fd;
+    size_t fsize;
+    const void *buf;
+    uint32_t base_mtime;
+};
+
+static uint32_t mtimedb_read32(const struct mtimedb *db, unsigned offset)
+{
+    return *(uint32_t *)((uint8_t *)db->buf + offset);
+}
+
+static int mtimedb_init(struct mtimedb *db, const char *fname, const char *prefix)
+{
+    struct stat st;
+    int rc = open(fname, O_RDONLY);
+
+    if(prefix[0]=='/') {
+        prefix++;
+    }
+    db->prefix = prefix;
+    db->prefix_len = strlen(prefix);
+
+    if(rc<0) {
+        perror(fname);
+        goto err_open;
+    }
+    db->fd = rc;
+    rc = fstat(db->fd, &st);
+    if(rc<0) {
+        perror(fname);
+        goto err_fstat;
+    }
+    db->fsize = st.st_size;
+    db->buf = mmap(NULL, db->fsize, PROT_READ, MAP_SHARED, db->fd, 0);
+    if(db->buf==MAP_FAILED) {
+        perror(fname);
+        rc = -1;
+        goto err_mmap;
+    }
+
+    db->base_mtime = mtimedb_read32(db, 8);
+
+    return 0;
+
+err_mmap:
+err_fstat:
+    close(db->fd);
+err_open:
+    return rc;
+}
+
+static void mtimedb_uninit(struct mtimedb *db)
+{
+    munmap((void *)db->buf, db->fsize);
+    close(db->fd);
+    return;
+}
+
+static int mtimedb_match_name(const struct mtimedb *db, unsigned offset, const char *path, unsigned *len)
+{
+    const char *db_name = (const char *)((uint8_t *)db->buf + offset);
+    unsigned i;
+    bool debug = false;
+    for(i=0;;) {
+        char db_ch = db_name[i];
+        char path_ch = path[i];
+        if(debug) {
+            printf("offset:%#x  %c %c\n", offset, path_ch, db_ch);
+        }
+        if(db_ch == path_ch) {
+            if(db_ch == 0) {
+                *len = i;
+                return 0;
+            }
+            i++;
+            continue;
+        }
+        if(path_ch=='/' &&  db_ch == 0 ) {
+            *len = i;
+            return 0;
+        }
+        return db_ch - path_ch;
+    }
+}
+
+struct name_entry {
+    uint32_t name;
+    uint32_t mtime;
+    uint32_t children;
+};
+
+struct mtimedb_lookup_level_result {
+    unsigned name_len;
+    const struct name_entry *entry;
+};
+
+static int mtimedb_lookup_level(const struct mtimedb *db, const char *path, unsigned offset, struct mtimedb_lookup_level_result *result)
+{
+    unsigned dirs = mtimedb_read32(db, offset);
+    unsigned i;
+    struct name_entry *entry;
+    bool debug=false;
+    offset += 4;
+    entry = (struct name_entry *)((uint8_t *)db->buf + offset);
+
+    for(i=0;i<dirs;i++) {
+        unsigned name_offset = entry[i].name;
+        if(debug) {
+            printf("i=%u dirs=%u\n", i, dirs);
+        }
+        if(mtimedb_match_name(db, name_offset, path, &result->name_len)==0) {
+            result->entry = entry+i;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+static int mtimedb_lookup(const struct mtimedb *db, const char *path, long *mtime)
+{
+    unsigned path_offset = 0;
+    unsigned db_offset = 16;
+    struct mtimedb_lookup_level_result result;
+    bool debug = false;
+
+    memset(&result, 0, sizeof(result));
+    if(debug) {
+        printf("prefux:%s(%u) path:%s cmp:%d\n", db->prefix, (unsigned)db->prefix_len, path, memcmp(path,db->prefix,db->prefix_len));
+    }
+    if(memcmp(path,db->prefix,db->prefix_len)!=0) {
+        return -1;
+    }
+    path=path+db->prefix_len;
+    for(;;) {
+        int rc = mtimedb_lookup_level(db, path+path_offset, db_offset, &result);
+        if(debug) {
+            printf("tc=%d offset:%#x match %s(%s)\n", rc, db_offset, path, path+(rc==0?result.name_len:0));
+        }
+        if(rc!=0) {
+            return rc;
+        }
+        path_offset += result.name_len;
+        if(path[path_offset]==0) {
+            *mtime = (long)db->base_mtime + result.entry->mtime;
+            break;
+        }
+        db_offset = result.entry->children;
+        if(db_offset==0) {
+            return -1;
+        }
+        path_offset ++;
+    }
+    return 0;
+}
+
+static const struct mtimedb *mtime_db=NULL;
+
+static void passthru_update_mtime(const char *path, struct stat *st)
+{
+    bool debug = false;
+
+    if(mtime_db) {
+        long mtime=st->st_mtime;
+        int rc = mtimedb_lookup(mtime_db, path+1, &mtime);
+        if(rc==0) {
+            if(debug) {
+                fprintf(stderr, "path:%s rc:%d\n", path, rc);
+            }
+            memset(&st->st_mtime, 0, sizeof(*st) - offsetof(struct stat, st_mtime));
+            st->st_mtime = mtime;
+            st->st_atime = st->st_mtime;
+            st->st_ctime = st->st_mtime;
+        }
+    }
+    return;
+}
+
 static int passthru_getattr(const char *path, struct stat *st)
 {
     CHECK(lstat(path, st));
+    passthru_update_mtime(path, st);
     return 0;
 }
+
 
 static int passthru_fgetattr(const char *path, struct stat *st,
                         struct fuse_file_info *fi)
 {
     CHECK(fstat(fi->fh, st));
-    UNUSED(path);
+    passthru_update_mtime(path, st);
     return 0;
 }
 
@@ -150,6 +335,81 @@ static const struct fuse_operations interposer_ops = {
 
 int main(int argc, char *argv[])
 {
+    struct mtimedb db;
+    int rc;
+    int cur_arg = 1;
+    bool mtime_verbose = false;
+    const char *mtime_prefix="/";
+    const char *db_path = NULL;
+    bool mtime_lookup = false;
+
+    for(;;) {
+        const char *arg;
+        if(cur_arg>=argc) {
+            break;
+        }
+        arg = argv[cur_arg];
+        if(cur_arg+1<argc && strcmp("--mtime",arg)==0) {
+            db_path = argv[cur_arg+1];
+            cur_arg+=2;
+            continue;
+        }
+        if(cur_arg+1<argc && strcmp("--mtime-prefix",arg)==0) {
+            mtime_prefix = argv[cur_arg+1];
+            cur_arg+=2;
+            continue;
+        }
+        if(strcmp("--mtime-verbose",arg)==0) {
+            mtime_verbose = true;
+            cur_arg++;
+            continue;
+        }
+        if(strcmp("--mtime-lookup",arg)==0) {
+            mtime_lookup=true;
+            cur_arg++;
+            continue;
+        }
+        break;
+    }
+    if(db_path) {
+        rc  = mtimedb_init(&db, db_path, mtime_prefix);
+        if(rc<0) {
+            return 1;
+        }
+        mtime_db=&db;
+    }
+    if(mtime_lookup) {
+        if(mtime_db==NULL) {
+            fprintf(stderr,"must speficy mtime database first\n");
+            return 1;
+        }
+        for(;;) {
+            char buf[256];
+            const char *path = fgets(buf,sizeof(buf),stdin);
+            long mtime;
+            if(path==NULL) {
+                break;
+            }
+            buf[strlen(path)-1]='\0';
+            rc = mtimedb_lookup(&db, path, &mtime);
+            if(mtime_verbose || rc<0) {
+                printf("%s %s\n", path, rc==0?"match":"no match");
+            }
+            if(rc<0) {
+                goto done;
+            }
+        }
+        goto done;
+    }
     umask(0);
-    return fuse_main(argc, argv, &interposer_ops, NULL);
+    if(cur_arg!=1) {
+        argv[cur_arg-1] = argv[0];
+    }
+    rc = fuse_main(argc-(cur_arg-1), argv+(cur_arg-1), &interposer_ops, NULL);
+done:
+    if(mtime_db) {
+        mtime_db = NULL;
+        mtimedb_uninit(&db);
+    }
+    return rc;
 }
